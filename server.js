e29193b62517d5ac57a23a69d5f938985ddb2144
oleksandrapoliakhova <oleksandraedwards@gmail.com>
2023-06-03 20:44:53 -0400

    heroku config

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const path = require('path');
 const app = express();
+app.use(express.static('public'));
 // /Users/poliakhova/foodapp-ui/foodapp-ui
-app.use(express.static(__dirname + '/dist/foodapp-ui/foodapp-ui'));
-app.get('/*', function (req, res) {
-  res.sendFile(path.join(__dirname +
-    // /Users/poliakhova/foodapp-ui/foodapp-ui/src/index.html
-    '/src/index.html'));
+app.use(express.static(__dirname + '/dist/foodapp-ui/'));
+app.get('*', function (req, res, next) {
+  res.sendFile('index.html', {root: './dist'});
+
 });
 app.listen(process.env.PORT || 8082);
