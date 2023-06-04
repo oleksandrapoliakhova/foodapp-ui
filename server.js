1f9dd2a56691d9932838d0a56f6d2f81ca2542dd
oleksandrapoliakhova <oleksandraedwards@gmail.com>
2023-06-03 20:02:09 -0400

    heroku config

--- server.js
+++ server.js
@@ -1,15 +1,19 @@
-//Install express server
-const express = require('express');
-const path = require('path');
+function requireHTTPS(req, res, next) {
+  // The 'x-forwarded-proto' check is for Heroku
+  if (!req.secure && req.get('x-forwarded-proto') !== 'https') {
+    return res.redirect('https://' + req.get('host') + req.url);
+  }
+  next();
+}
 
+const express = require('express');
 const app = express();
 
-// Serve only the static files form the dist directory
-app.use(express.static('./dist/angular-app-heroku'));
+app.use(requireHTTPS);
+app.use(express.static('./dist/angular-heroku'));
 
 app.get('/*', (req, res) =>
-  res.sendFile('index.html', {root: 'dist/angular-app-heroku/'}),
+  res.sendFile('index.html', {root: 'dist/foodapp-ui/'}),
 );
 
-// Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8080);
+app.listen(process.env.PORT || 8081);
